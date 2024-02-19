32fa62f8dad1eaaf2d35d6f0a1659091f644af25
Alexander <alexander_hh@tesch.edu.mx>
2024-02-18 19:52:00 -0600

    letter animation

---
+++ script.js
@@ -0,0 +1,14 @@
+const spans = document.querySelectorAll('.word span');
+
+spans.forEach((span, idx) => {
+	span.addEventListener('click', (e) => {
+		e.target.classList.add('active');
+	});
+	span.addEventListener('animationend', (e) => {
+		e.target.classList.remove('active');
+	});
+	
+	setTimeout(() => {
+		span.classList.add('active');
+	}, 750 * (idx+1))
+});
